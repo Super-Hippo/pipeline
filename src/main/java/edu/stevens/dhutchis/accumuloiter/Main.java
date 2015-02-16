91c57797d8cc58bfd1d9523a5ce440c874403db8
ericcherin <eric.cherin@gmail.com>
2015-02-16 10:19:59 -0500

    h

---
+++ src/main/java/edu/stevens/dhutchis/accumuloiter/Main.java
@@ -0,0 +1,111 @@
+
+package edu.stevens.dhutchis.accumuloiter;
+
+import org.apache.accumulo.core.client.*;
+import org.apache.accumulo.core.client.Scanner;
+import org.apache.accumulo.core.data.Key;
+import org.apache.accumulo.core.data.Mutation;
+import org.apache.accumulo.core.data.Value;
+import org.apache.accumulo.core.iterators.Combiner;
+import org.apache.accumulo.core.iterators.IteratorUtil;
+import org.apache.accumulo.core.iterators.LongCombiner;
+import org.apache.accumulo.core.iterators.user.SummingCombiner;
+import org.apache.accumulo.core.security.Authorizations;
+import org.apache.hadoop.io.Text;
+import org.junit.Assert;
+
+import java.util.*;
+import java.util.Map.Entry;
+
+/**
+ *
+ * @author dhutchis
+ */
+public class Main {
+    private final String tableName = "TestTableIterator";
+    private final String columnFamily="";
+    private final String columnVisibility="";
+    
+    public static void main(String[] args)
+    {
+		System.out.println("HI im in main");
+	}
+	
+    public void testIter(Connector conn) throws AccumuloException, AccumuloSecurityException, TableNotFoundException, TableExistsException {
+        
+        /*Text rowID = new Text("row1");
+        Text colFam = new Text("myColFam");
+        Text colQual = new Text("myColQual");
+        ColumnVisibility colVis = new ColumnVisibility();
+        long timestamp = System.currentTimeMillis();
+        Value value = new Value("myValue".getBytes());
+        Mutation mutation = new Mutation(rowID);
+        mutation.put(colFam, colQual, colVis, timestamp, value);*/
+        if (conn.tableOperations().exists(tableName))
+            conn.tableOperations().delete(tableName);
+        conn.tableOperations().create(tableName);
+        
+        String iterName = "summingIter";
+		
+        // Setup IteratorSetting
+        IteratorSetting cfg = new IteratorSetting(1, iterName, SummingCombiner.class);
+        LongCombiner.setEncodingType(cfg, LongCombiner.Type.STRING);
+        // add columns to act on
+        List<IteratorSetting.Column> combineColumns = new ArrayList<>();
+        combineColumns.add(new IteratorSetting.Column(columnFamily, "leg"));
+        Combiner.setColumns(cfg, combineColumns);
+
+        // Add Iterator to table
+        conn.tableOperations().attachIterator(tableName, cfg);
+        // Verify successful add
+        Map<String,EnumSet<IteratorUtil.IteratorScope>> iterMap = conn.tableOperations().listIterators(tableName);
+        EnumSet<IteratorUtil.IteratorScope> iterScope = iterMap.get(iterName);
+        Assert.assertNotNull(iterScope);
+        Assert.assertTrue(iterScope.containsAll(EnumSet.allOf(IteratorUtil.IteratorScope.class)));
+        
+        Text row1 = new Text("row1");
+        Text cqleg = new Text("leg");
+        Value[] vlegs = new Value[] {new Value("3".getBytes()), new Value("4".getBytes()), new Value("5".getBytes())  };
+
+        // AH HA!! If you batch writes to the same key together, only the last one actually transmits to Accumulo.
+        // Lesson: do each write separately.
+//        Mutation m1 = new Mutation(row1);
+//        for (Value vleg : vlegs)
+//            m1.put(new Text(columnFamily), cqleg, vleg);
+        
+        BatchWriterConfig config = new BatchWriterConfig();
+        BatchWriter writer = conn.createBatchWriter(tableName, config);
+//        writer.addMutation(m1);
+//        writer.flush();
+
+        for (Value vleg : vlegs) {
+            Mutation m1 = new Mutation(row1);
+            m1.put(new Text(columnFamily), cqleg, vleg);
+            writer.addMutation(m1);
+            writer.flush();
+        }
+        
+        // check results
+        Scanner scan = conn.createScanner(tableName, Authorizations.EMPTY);
+        System.out.println("Scanner range: "+scan.getRange());
+        for(Entry<Key,Value> entry : scan) {
+            System.out.println(entry);
+            Assert.assertEquals("12", entry.getValue().toString());
+        }
+System.out.println("HI56");
+        // try a custom iterator...
+        
+    }
+    
+    class ConcatIter extends Combiner {
+        @Override
+        public Value reduce(Key key, Iterator<Value> iter) {
+            String ss = "";
+            while (iter.hasNext()) {
+                Value v = iter.next();
+                ss += v.toString();
+            }
+            return new Value(ss.getBytes());
+        }
+    }
+}
---
+++ src/test/java/edu/stevens/dhutchis/accumuloiter/MainTest.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.stevens.dhutchis.accumuloiter;
+
+import org.apache.accumulo.core.client.ClientConfiguration;
+import org.apache.accumulo.core.client.Connector;
+import org.apache.accumulo.core.client.Instance;
+import org.apache.accumulo.core.client.ZooKeeperInstance;
+import org.apache.accumulo.core.client.security.tokens.PasswordToken;
+import org.apache.accumulo.minicluster.MiniAccumuloCluster;
+import org.junit.*;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.File;
+import java.util.Collection;
+
+/**
+ *
+ * @author dhutchis
+ */
+public class MainTest {
+    @Rule
+    public TemporaryFolder tempFolder = new TemporaryFolder();
+
+    private String username = "root";
+    private String password = "secret";
+    private static ClientConfiguration myconfig;
+    static {
+        String instance = "instance";
+        String host = "b412srv.ece.stevens-tech.edu:2181";
+        int timeout = 100000;
+        myconfig = new ClientConfiguration().withInstance(instance).withZkHosts(host).withZkTimeout(timeout);
+    }
+
+    private void printList(Collection<?> list, String prefix) {
+        System.out.print(prefix+": ");
+        for (Object o : list) {
+            System.out.print(o+", ");
+        }
+        System.out.println();
+    }
+    
+    public MainTest() {
+	
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testMini() throws Exception {
+
+        File tempDir = tempFolder.newFolder();
+        MiniAccumuloCluster accumulo = new MiniAccumuloCluster(tempDir, "password");
+        accumulo.start(); // doesn't work on Dylan's computer for some reason.  The OS closes the Zookeeper connection.
+System.out.println("HI1");
+        Instance instance = new ZooKeeperInstance(accumulo.getInstanceName(), accumulo.getZooKeepers());
+        Connector conn = instance.getConnector("root", new PasswordToken("password"));
+
+        innerTest(instance, conn);
+System.out.println("HI2");
+        accumulo.stop();
+        tempDir.delete();
+    }
+
+    @Test
+    public void testNormal() throws Exception {
+        Instance instance = new ZooKeeperInstance(myconfig.get(ClientConfiguration.ClientProperty.INSTANCE_NAME), myconfig.get(ClientConfiguration.ClientProperty.INSTANCE_ZK_HOST));
+        System.out.println("made instance : "+instance);
+        Connector conn = instance.getConnector(username, new PasswordToken(password));
+        System.out.println("made connector: "+conn);
+
+        innerTest(instance, conn);
+    }
+
+    private void innerTest(Instance instance, Connector conn) throws Exception {
+        printList(conn.tableOperations().list(), "tables");
+        printList(instance.getMasterLocations(), "master_locations");
+System.out.println("HI3");
+        Main main = new Main();
+        main.testIter(conn);
+    }
+	
+	public static void main(String[] args)
+    {
+		System.out.println("HI im in main");
+	}
+}
