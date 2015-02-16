67b77b37269e434aa69c1fbc8b7248cf0751a66d
ericcherin <eric.cherin@gmail.com>
2015-02-16 10:31:38 -0500

    sdf

--- src/main/java/edu/stevens/dhutchis/accumuloiter/Main.java
+++ src/main/java/edu/stevens/dhutchis/accumuloiter/Main.java
@@ -28,7 +28,7 @@ public class Main {
     
     public static void main(String[] args)
     {
-		System.out.println("HI im in main");
+		System.out.println("HI im in main i updated");
 	}
 	
     public void testIter(Connector conn) throws AccumuloException, AccumuloSecurityException, TableNotFoundException, TableExistsException {
