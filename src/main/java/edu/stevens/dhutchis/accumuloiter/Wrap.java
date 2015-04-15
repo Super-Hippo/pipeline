ea0d95934bbcb8a4c1df3f67cb4125b8aac4bcf8
ericcherin <eric.cherin@gmail.com>
2015-04-15 10:47:19 -0400

    sdf

--- src/main/java/edu/stevens/dhutchis/accumuloiter/Wrap.java
+++ src/main/java/edu/stevens/dhutchis/accumuloiter/Wrap.java
@@ -139,7 +139,10 @@ public class Wrap {
                     String seq = batEntry.getValue().toString();
                     String mykey = batEntry.getKey().toString();
                     rawSeq.put(mykey,seq);
-                    //System.out.println("co is: : " + co + " value is : " + seq + "\n");
+                    if(counter < 11500 && counter > 114990)
+                    {
+                        System.out.println("co is: : " + co + " value is : " + seq + "\n");
+                    }
                     co++;
 
                 }
